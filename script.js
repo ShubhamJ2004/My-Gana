9370696d5e5d4769aee8caa768c3bd5b3337b5ae
ShubhamJ2004 <88619809+ShubhamJ2004@users.noreply.github.com>
2023-08-08 17:35:57 +0530

    Add files via upload
    this is my sample files of my very big project!!!!!

---
+++ script.js
@@ -0,0 +1,47 @@
+let music1 = document.querySelector('.music1');
+music1.addEventListener('click', function(){
+    let audio = document.createElement('audio');
+    audio.setAttribute('src', 'Tu Hai.mp3');
+    // audio.loop = true;
+    audio.play();
+})
+
+let music2 = document.querySelector('.music2');
+music2.addEventListener('click', function(){
+    let audio = document.createElement('audio');
+    audio.setAttribute('src', 'Where are you now.mp3');
+    // audio.loop = true;
+    audio.play();
+})
+
+let music3 = document.querySelector('.music3');
+music3.addEventListener('click', function(){
+    let audio = document.createElement('audio');
+    audio.setAttribute('src', 'Hymn For The Weekend.mp3');
+    // audio.loop = true;
+    audio.play();
+})
+
+let music4 = document.querySelector('.music4');
+music4.addEventListener('click', function(){
+    let audio = document.createElement('audio');
+    audio.setAttribute('src', 'Hall of Fame.mp3');
+    // audio.loop = true;
+    audio.play();
+})
+
+let music5 = document.querySelector('.music5');
+music5.addEventListener('click', function(){
+    let audio = document.createElement('audio');
+    audio.setAttribute('src', 'Inspire.mp3');
+    // audio.loop = true;
+    audio.play();
+})
+
+let music6 = document.querySelector('.music6');
+music6.addEventListener('click', function(){
+    let audio = document.createElement('audio');
+    audio.setAttribute('src', 'Aarambh Hai Prachand.mp3');
+    // audio.loop = true;
+    audio.play();
+})
